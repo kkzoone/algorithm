6819788ff29471b3401ed1b20650b92a126cc117
kim-kyung-jun <91443538+kkzoone@users.noreply.github.com>
2023-05-14 15:57:33 +0900

    [Silver I] Title: 완전 이진 트리, Time: 320 ms, Memory: 21808 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+
+        // 트리 노드 개수
+        int k = (int)Math.pow(2,scanner.nextInt())-1;
+
+        // 인오더 탐색의 결과로 방문한 노드
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            l.add(scanner.nextInt());
+        }
+
+        // 인오터 탐색의 결과로 트리를 만듦
+
+        Node root = Main.buildTree(l,0,l.size()-1);
+        printTree(root);
+    }
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    static Node buildTree(List<Integer> inorder , int start , int end){
+        if (start > end){
+            return null;
+        }
+        int i = (start + end) / 2;
+
+        // root 노드
+        Node node = new Node(inorder.get(i));
+
+        // 말단 노드
+        if (start == end) {
+            return node;
+        }
+
+        node.left = buildTree(inorder, start, i - 1);
+        node.right = buildTree(inorder, i + 1, end);
+
+        return node;
+    }
+
+    static void printTree(Node root){
+        StringBuilder result = new StringBuilder();
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                Node node = queue.poll();
+                result.append(node.data + " ");
+                if (node.left!=null) queue.add(node.left);
+                if (node.right!=null) queue.add(node.right);
+            }
+            result.append("\n");
+        }
+        System.out.println(result.toString());
+    }
+}
