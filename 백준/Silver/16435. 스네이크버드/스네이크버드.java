0239772b486eee5163dc4ac235a4a28e24b1f14d
kim-kyung-jun <91443538+kkzoone@users.noreply.github.com>
2023-05-17 14:40:04 +0900

    [Silver V] Title: 스네이크버드, Time: 264 ms, Memory: 19940 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int snake = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (snake >=arr[i]){
+                snake++;
+            }
+        }
+
+        System.out.println(snake);
+    }
+}
