fc7763b211b428f5f64821f0bd8a8ad81db58eb5
kim-kyung-jun <91443538+kkzoone@users.noreply.github.com>
2023-05-23 10:20:14 +0900

    [Silver V] Title: 수들의 합 5, Time: 160 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,31 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(System.in)));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int count = 1; // count가 1인 이유는 end가 N일 때를 미리 고려한 것
-        int sum = 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        int sum = 0;
         int start = 1;
         int end = 1;
-        while(end!=N){
-            if(sum==N){ // sum과 N이 같다면
-                count++; // count + 1
-                end++; // end 옆으로 이동 
-                sum = sum+end; // end 이동했으므로 합이 커짐
-            } else if(sum > N){ // sum이 N보다 크다면
-                sum = sum-start; // 값을 줄여줘야 하기 때문에 
-                start++; // start 옆으로 이동
-            } else{ // sum이 N보다 작다면
-                end++; // 값을 더해줘야 하기 때문에
-                sum = sum+end; // end 옆으로 이동
+
+        while (start <= N) {
+            if (sum < N) {
+                sum += end;
+                end++;
+            } else if (sum > N) {
+                sum -= start;
+                start++;
+            } else {
+                count++;
+                sum += end;
+                end++;
             }
         }
+
         System.out.println(count);
     }
 }
