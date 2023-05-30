9cab4672822a7962dfe3b729ec8d1c8cd1c2d187
kim-kyung-jun <91443538+kkzoone@users.noreply.github.com>
2023-05-30 10:51:47 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,53 +1,34 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
- 
-	static Scanner in = new Scanner(System.in);
- 
-	public static void main(String[] args) {
- 
-		int count = 0;
-		int N = in.nextInt();
- 
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		int N = Integer.parseInt(br.readLine());
+
+		int cnt = N;
+
 		for (int i = 0; i < N; i++) {
-			if (check() == true) {
-				count++;
+			boolean[] check = new boolean[26];
+			String s = br.readLine();
+			check[s.charAt(0) - 'a'] = true;
+
+			for (int j = 1; j < s.length(); j++) {
+
+				if (s.charAt(j) != s.charAt(j - 1)) {
+					if (check[s.charAt(j) - 'a']) {
+						cnt--;
+						break;
+					} else {
+						check[s.charAt(j) - 'a'] = true;
+					}
+				}
+
 			}
 		}
-		System.out.println(count);
+		System.out.println(cnt);
 	}
- 
-	public static boolean check() {
-		boolean[] check = new boolean[26];
-		int prev = 0;
-		String str = in.next();
-		
-		for(int i = 0; i < str.length(); i++) {
-			int now = str.charAt(i);	// i 번째 문자 저장 (현재 문자)
-			
-			
-			// 앞선 문자와 i 번째 문자가 같지 않다면?
-			if (prev != now) {		
-				
-				// 해당 문자가 처음 나오는 경우 (false 인 경우)
-				if ( check[now - 'a'] == false ) {
-					check[now - 'a'] = true;		// true 로 바꿔준다
-					prev = now;					// 다음 턴을 위해 prev 도 바꿔준다 
-				}
-	 
-				// 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨) 
-				else {
-					return false;	//함수 종료
-				}
-			}
-	        
-	        
-			// 앞선 문자와 i 번째 문자가 같다면? (연속된 문자)
-			// else 문은 없어도 됨
-			else {
-				continue;
-			}
-		}    
-		return true;
-	}
+
 }
+
+
